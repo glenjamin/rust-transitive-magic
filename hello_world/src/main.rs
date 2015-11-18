547607651f607644395cf1bc709f50c9db3f6bfa
Glen Mailer <glenjamin@gmail.com>
2015-11-18 00:29:07 +0000

    it works!

---
+++ hello_world/src/main.rs
@@ -0,0 +1,6 @@
+extern crate magic_number_a;
+extern crate magic_number_b;
+
+fn main() {
+    println!("A: {}, B: {}", magic_number_a::num(), magic_number_b::num())
+}
---
+++ magic-number-b/src/lib.rs
@@ -0,0 +1,6 @@
+extern crate magic_number;
+use magic_number::n;
+
+pub fn num() -> i64 {
+    n()
+}
